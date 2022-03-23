614af214bdd1472695eda2a9191912af912e5f2e
cnietoza <hanna.nieto@hotmail.com>
2022-03-22 20:24:55 -0500

    DIPLOMADO

---
+++ script.js
@@ -0,0 +1,206 @@
+$(".1").css("color","blue")
+
+$(".1").click( function(){
+
+    $(".1").css("font-size", "20px")    
+ })
+
+ $(".evento1").dblclick( function(){
+
+    $(".evento1").css("font-size", "10px")
+})
+
+
+
+$(".2").css("color","purple")
+
+$(".2").click( function(){
+
+    $(".2").css("font-size", "20px")    
+ })
+
+$(".evento2").dblclick( function(){
+
+    $(".evento2").css("font-size", "10px")
+})
+
+
+$(".3").css("color","red")
+
+$(".3").click( function(){
+
+    $(".3").css("font-size", "20px")    
+ })
+
+$(".evento3").dblclick( function(){
+
+    $(".evento3").css("font-size", "10px")
+})
+
+
+$(".4").css("color","green")
+
+$(".4").click( function(){
+
+    $(".4").css("font-size", "20px")    
+ })
+
+$(".evento4").dblclick( function(){
+
+    $(".evento4").css("font-size", "10px")
+})
+
+
+
+
+$(".5").css("color","orange")
+
+$(".5").click( function(){
+
+    $(".5").css("font-size", "20px")    
+ })
+
+$(".evento5").dblclick( function(){
+
+    $(".evento5").css("font-size", "10px")
+})
+
+
+$(".6").css("color","red")
+
+$(".6").click( function(){
+
+    $(".6").css("font-size", "20px")    
+ })
+
+$(".evento6").dblclick( function(){
+
+    $(".evento6").css("font-size", "10px")
+})
+
+$(".7").css("color","green")
+
+$(".7").click( function(){
+
+    $(".7").css("font-size", "20px")    
+ })
+
+$(".evento7").dblclick( function(){
+
+    $(".evento7").css("font-size", "10px")
+})
+
+
+
+$(".8").css("color","olive")
+
+$(".8").click( function(){
+
+    $(".8").css("font-size", "20px")    
+ })
+
+$(".evento8").dblclick( function(){
+
+    $(".evento8").css("font-size", "10px")
+})
+
+
+
+$(".9").css("color","black")
+
+$(".9").click( function(){
+
+    $(".9").css("font-size", "20px")    
+ })
+
+$(".evento9").dblclick( function(){
+
+    $(".evento9").css("font-size", "10px")
+})
+
+
+$(".10").css("color","red")
+
+$(".10").click( function(){
+
+    $(".10").css("font-size", "20px")    
+ })
+
+ $(".evento10").dblclick( function(){
+
+    $(".evento10").css("font-size", "10px")
+})
+
+
+
+$(".caja1").mouseover(function(){
+
+    $(".caja1").css("background", "salmon")
+})
+
+/* SALGA DEL PARRAFO, SE VUELVA FONDO AZUL
+
+mouseout*/
+
+$(".caja1").mouseout(function(){
+
+    $(".caja1").css("background", "aqua")
+})
+
+
+
+
+
+
+
+$(".caja2").mouseover(function(){
+
+    $(".caja2").css("background", "orange")
+})
+
+
+$(".caja2").mouseout(function(){
+
+    $(".caja2").css("background", "green")
+})
+
+
+
+$(".caja3").mouseover(function(){
+
+    $(".caja3").css("background", "pink")
+})
+
+
+$(".caja3").mouseout(function(){
+
+    $(".caja3").css("background", "yellow")
+})
+
+
+$(".caja4").mouseover(function(){
+
+    $(".caja4").css("background", "pink")
+})
+
+
+$(".caja4").mouseout(function(){
+
+    $(".caja4").css("background", "yellow")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
